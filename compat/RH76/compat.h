ab4406ac6c1dcb77d47e5df9f3b853bcefcaaf5c
Kaike Wan <kaike.wan@intel.com>
2024-07-17 09:12:16 -0400

    This is the 11.7 release of the Rendezvous Module for Intel Ethernet Fabric Suite

---
+++ compat/RH76/compat.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause) */
+/*
+ * Copyright(c) 2020 Intel Corporation.
+ */
+#if !defined(RH76_COMPAT_H)
+#define RH76_COMPAT_H
+
+#define CREATE_AH_HAS_UDATA
+#define HAVE_ALLOC_RDMA_NETDEV
+
+#include "compat_common.h"
+
+#endif //RH76_COMPAT
